e03defcacc1a605302c8fae6b855aa2223b48d19
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2021-03-13 21:59:50 +0530

    Tacotron dataset

--- seq2seq/dataset.py
+++ seq2seq/dataset.py
@@ -30,56 +30,56 @@ def _load_training_instances(filename):
     return training_instances
 
 
-class SortedSampler(samplers.Sampler):
-    """Sorted sampler
-    """
-    def __init__(self, data, sort_key):
-        """Instantiate the sampler
-        """
-        super().__init__(data)
+# class SortedSampler(samplers.Sampler):
+#     """Sorted sampler
+#     """
+#     def __init__(self, data, sort_key):
+#         """Instantiate the sampler
+#         """
+#         super().__init__(data)
 
-        self.data = data
-        self.sort_key = sort_key
+#         self.data = data
+#         self.sort_key = sort_key
 
-        zip_ = [(i, self.sort_key(row)) for i, row in enumerate(self.data)]
-        zip_ = sorted(zip_, key=lambda r: r[1], reverse=True)
-        self.sorted_indexes = [item[0] for item in zip_]
+#         zip_ = [(i, self.sort_key(row)) for i, row in enumerate(self.data)]
+#         zip_ = sorted(zip_, key=lambda r: r[1], reverse=True)
+#         self.sorted_indexes = [item[0] for item in zip_]
 
-    def __iter__(self):
-        return iter(self.sorted_indexes)
+#     def __iter__(self):
+#         return iter(self.sorted_indexes)
 
-    def __len__(self):
-        return len(self.data)
+#     def __len__(self):
+#         return len(self.data)
 
 
-class BucketBatchSampler(samplers.BatchSampler):
-    """Bucket batch sampler
-    """
-    def __init__(self, sampler, sort_key, batch_size, bucket_size_multiplier,
-                 drop_last):
-        """Instantiate the Sampler
-        """
-        super().__init__(sampler, batch_size, drop_last)
+# class BucketBatchSampler(samplers.BatchSampler):
+#     """Bucket batch sampler
+#     """
+#     def __init__(self, sampler, sort_key, batch_size, bucket_size_multiplier,
+#                  drop_last):
+#         """Instantiate the Sampler
+#         """
+#         super().__init__(sampler, batch_size, drop_last)
 
-        self.sort_key = sort_key
-        self.bucket_sampler = samplers.BatchSampler(
-            sampler, min(batch_size * bucket_size_multiplier, len(sampler)),
-            False)
+#         self.sort_key = sort_key
+#         self.bucket_sampler = samplers.BatchSampler(
+#             sampler, min(batch_size * bucket_size_multiplier, len(sampler)),
+#             False)
 
-    def __iter__(self):
-        for bucket in self.bucket_sampler:
-            sorted_sampler = SortedSampler(bucket, self.sort_key)
-            for batch in samplers.SubsetRandomSampler(
-                    list(
-                        samplers.BatchSampler(sorted_sampler, self.batch_size,
-                                              self.drop_last))):
-                yield [bucket[i] for i in batch]
+#     def __iter__(self):
+#         for bucket in self.bucket_sampler:
+#             sorted_sampler = SortedSampler(bucket, self.sort_key)
+#             for batch in samplers.SubsetRandomSampler(
+#                     list(
+#                         samplers.BatchSampler(sorted_sampler, self.batch_size,
+#                                               self.drop_last))):
+#                 yield [bucket[i] for i in batch]
 
-    def __len__(self):
-        if self.drop_last:
-            return len(self.sampler) // self.batch_size
-        else:
-            return math.ceil(len(self.sampler) / self.batch_size)
+#     def __len__(self):
+#         if self.drop_last:
+#             return len(self.sampler) // self.batch_size
+#         else:
+#             return math.ceil(len(self.sampler) / self.batch_size)
 
 
 class TTSDataset(Dataset):
