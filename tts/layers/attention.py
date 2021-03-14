7242cba68bb41426133d65c8337118fe347362ac
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2021-03-14 15:53:02 +0530

    Attention layers

--- tts/layers/attention.py
+++ tts/layers/attention.py
@@ -35,9 +35,6 @@ class DynamicConvolutionAttention(nn.Module):
         self.alpha = alpha
         self.beta = beta
 
-        self.attention_weights = None
-        self._mask_value = 1e-8
-
         self.query_layer = nn.Linear(query_dim, attn_dim)
         self.key_layer = nn.Linear(attn_dim,
                                    dynamic_channels * dynamic_kernel_size,
@@ -59,26 +56,18 @@ class DynamicConvolutionAttention(nn.Module):
         prior = betabinom.pmf(range(prior_len), prior_len - 1, alpha, beta)
         self.register_buffer("prior", torch.FloatTensor(prior).flip(0))
 
-    def _init_states(self, memory):
-        """Initialize the attention states
-        """
-        B, T = memory.size(0), memory.size(1)
-
-        self.attention_weights = torch.zeros([B, T], device=memory.device)
-        self.attention_weights[:, 0] = 1
-
-    def forward(self, query, memory, mask):
+    def forward(self, query, attention_weights):
         """Forward pass
         """
         # Compute prior filter
         prior_filter = F.conv1d(
-            F.pad(self.attention_weights.unsqueeze(1),
-                  (self.prior_len - 1, 0)), self.prior.view(1, 1, -1))
+            F.pad(attention_weights.unsqueeze(1), (self.prior_len - 1, 0)),
+            self.prior.view(1, 1, -1))
         prior_filter = torch.log(prior_filter.clamp_min_(1e-6)).squeeze(1)
 
         # Compute dynamic filter
         G = self.key_layer(torch.tanh(self.query_layer(query)))
-        dynamic_filter = F.conv1d(self.attention_weights.unsqueeze(0),
+        dynamic_filter = F.conv1d(attention_weights.unsqueeze(0),
                                   G.view(-1, 1, self.dynamic_kernel_size),
                                   padding=(self.dynamic_kernel_size - 1) // 2,
                                   groups=query.size(0))
@@ -88,22 +77,13 @@ class DynamicConvolutionAttention(nn.Module):
 
         # Compute static filter
         static_filter = self.static_filter(
-            self.attention_weights.unsqueeze(1)).transpose(1, 2).contiguous()
+            attention_weights.unsqueeze(1)).transpose(1, 2).contiguous()
 
         # Compute alignment and attention weights
-        alignment = self.v(
+        score = self.v(
             torch.tanh(
                 self.static_filter_layer(static_filter) +
                 self.dynamic_filter_layer(dynamic_filter))).squeeze(
                     -1) + prior_filter
-        attention_weights = F.softmax(alignment, dim=-1)
 
-        # Apply masking
-        if mask is not None:
-            attention_weights.data.masked_fill_(~mask, self._mask_value)
-        self.attention_weights = attention_weights
-
-        # Compute attention context
-        context = torch.bmm(attention_weights.unsqueeze(1), memory).squeeze(1)
-
-        return context
+        return F.softmax(score, dim=-1)
