4d8fd3ace0f3ea83ac900b7163fb894e52073535
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2021-03-16 22:05:55 +0530

    TTS dataset

--- tts/dataset.py
+++ tts/dataset.py
@@ -1,14 +1,10 @@
 """Tacotron dataset"""
 
-import math
 import os
 
 import numpy as np
 import torch
-import torch.nn.functional as F
-import torch.utils.data.sampler as samplers
 from text.english import load_cmudict, symbol_to_id, text_to_id
-from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
 
 
@@ -29,63 +25,34 @@ def _load_training_instances(filename):
     return training_instances
 
 
-class SortedSampler(samplers.Sampler):
-    """Adapted from https://github.com/PetrochukM/PyTorch-NLP
+def _pad_text(text_seq, max_len):
+    """Pad a 1-D sequence
     """
-    def __init__(self, data, sort_key):
-        super().__init__(data)
-
-        self.data = data
-        self.sort_key = sort_key
-        zip_ = [(i, self.sort_key(row)) for i, row in enumerate(self.data)]
-        zip_ = sorted(zip_, key=lambda r: r[1], reverse=True)
-
-        self.sorted_indices = [item[0] for item in zip_]
-
-    def __len__(self):
-        return len(self.data)
-
-    def __iter__(self):
-        return iter(self.sorted_indices)
+    return np.pad(text_seq, (0, max_len - len(text_seq)),
+                  mode="constant",
+                  constant_values=symbol_to_id["_PAD_"])
 
 
-class BucketBatchSampler(samplers.BatchSampler):
-    """Adapted from https://github.com/PetrochukM/PyTorch-NLP
+def _pad_mel(mel, max_len):
+    """Pad a 2-D sequence
     """
-    def __init__(self, sampler, batch_size, drop_last, sort_key,
-                 bucket_size_multiplier):
-        super().__init__(sampler, batch_size, drop_last)
+    x = np.pad(mel, [(0, max_len - len(mel)), (0, 0)],
+               mode="constant",
+               constant_values=0)
 
-        self.sort_key = sort_key
-        self.bucket_sampler = samplers.BatchSampler(
-            sampler, min(batch_size * bucket_size_multiplier, len(sampler)),
-            False)
-
-    def __iter__(self):
-        for bucket in self.bucket_sampler:
-            sorted_sampler = SortedSampler(bucket, self.sort_key)
-            for batch in samplers.SubsetRandomSampler(
-                    list(
-                        samplers.BatchSampler(sorted_sampler, self.batch_size,
-                                              self.drop_last))):
-                yield [bucket[i] for i in batch]
-
-    def __len__(self):
-        if self.drop_last:
-            return len(self.sampler) // self.batch_size
-        else:
-            return math.ceil(len(self.sampler) / self.batch_size)
+    return x
 
 
 class TTSDataset(Dataset):
     """TTS dataset
     """
-    def __init__(self, train_data_dir):
+    def __init__(self, train_data_dir, reduction_factor):
         """Instantiate the dataset
         """
         super().__init__()
 
         self.train_data_dir = train_data_dir
+        self.reduction_factor = reduction_factor
 
         self.training_instances = _load_training_instances(
             os.path.join(train_data_dir, "metadata_train.txt"))
@@ -94,9 +61,6 @@ class TTSDataset(Dataset):
         self.max_length_index = np.argmax(self.lengths)
 
         self.cmudict = load_cmudict()
-
-    def sort_key(self, index):
-        return self.lengths[index]
 
     def __len__(self):
         return len(self.training_instances)
@@ -110,27 +74,29 @@ class TTSDataset(Dataset):
 
         text = text_to_id(text, self.cmudict)
 
-        return (torch.FloatTensor(mel.T), torch.LongTensor(text),
-                index == self.max_length_index)
+        return text, mel.T
 
+    def collate(self, batch):
+        """Collate and create padded batches
+        """
+        text_lengths = [len(x[0]) for x in batch]
+        mel_lengths = [len(x[1]) for x in batch]
 
-def collate(batch):
-    """Collate and create padded batches
-    """
-    mels, texts, attn_flags = zip(*batch)
+        max_text_len = np.max(text_lengths)
+        max_mel_len = np.max(mel_lengths) + 1
 
-    mels, texts = list(mels), list(texts)
+        if max_mel_len % self.reduction_factor != 0:
+            max_mel_len += self.reduction_factor - max_mel_len % self.reduction_factor
+            assert max_mel_len % self.reduction_factor == 0
 
-    # Pad the batch to a muliple of reduction factor
-    mels[0] = F.pad(mels[0], (0, 0, 0, 0))
+        texts = np.array([_pad_text(x[0], max_text_len) for x in batch],
+                         dtype=np.int)
+        texts = torch.LongTensor(texts)
+        text_lengths = torch.LongTensor(text_lengths)
 
-    mel_lengths = [len(mel) for mel in mels]
-    text_lengths = [len(text) for text in texts]
+        mels = np.array([_pad_mel(x[1], max_mel_len) for x in batch],
+                        dtype=np.float32)
+        mels = torch.FloatTensor(mels)
+        mel_lengths = torch.LongTensor(mel_lengths)
 
-    mels = pad_sequence(mels, batch_first=True)
-    texts = pad_sequence(texts,
-                         batch_first=True,
-                         padding_value=symbol_to_id["_PAD_"])
-    attn_flags = [idx for idx, flag in enumerate(attn_flags) if flag]
-
-    return texts, text_lengths, mels.transpose_(1, 2), mel_lengths, attn_flags
+        return texts, text_lengths, mels, mel_lengths
